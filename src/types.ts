376a413a0648833c22e8541706c1c1e240011969
Varun Singh <varunsingh9114@gmail.com>
2024-12-10 21:42:29 +0530

    types: Create a separate module to declare types.

---
+++ src/types.ts
@@ -0,0 +1,50 @@
+export type TokenParams = {
+    access_token: string,
+    refresh_token: string,
+    expires_at: number,
+    token_id: string,
+    token_type: string
+}
+
+export type RefreshTokenRequestBody = {
+    grant_type: "refresh_token",
+    client_id: string,
+    client_secret?: string,
+    refresh_token: string,
+    scope?: string,
+}
+
+export type RefreshTokenResponseBody = {
+    access_token: string,
+    expires_in: number,
+    scope: string,
+    token_id: string,
+    token_type: string,
+}
+
+export type OAuthClientConfigParams = {
+    client_id: string,
+    client_secret: string,
+    redirect_uri: string,
+    connection: string,
+    scope?: string,
+    response_type?: string,
+    organisation?: string,
+}
+
+export type CallbackParams = {
+    authorization_code: string,
+    state: string,
+}
+
+export type CallbackRequestParams = {
+    grant_type: "authorization_code",
+    client_id: string,
+    code: string,
+    code_verifier: string,
+    redirect_uri: string,
+}
+
+export type CallbackResponse = TokenParams;
+
+export type RuntimeEnv = "browser" | "node";
